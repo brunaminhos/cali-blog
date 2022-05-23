98f37958cbdadf8db17dd0b94c1794dd802253af
Bruna Minhos <bruna.minhosdossantos@student.griffith.ie>
2022-05-23 23:55:20 +0100

    Changed: port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const flash = require('connect-flash');
 
 const app = express();
 // const port = process.env.PORT || 3000;
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 require('dotenv').config();
 
@@ -31,7 +31,4 @@ app.set('view engine', 'ejs');
 const routes = require('./server/routes/caliRoutes.js')
 app.use('/', routes);
 
-// app.listen(port, ()=> console.log('Listening to port ${port}'));
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(port, ()=> console.log('Listening to port ${port}'));
